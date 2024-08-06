680493cfbcf2c444ecc65a5d8f3a26b5e9222939
ivan_holyk <ivan.Holyk369@gmail.com>
2024-08-06 14:25:44 +0300

    add spiner to market view

--- src/views/MarketView.vue
+++ src/views/MarketView.vue
@@ -1,14 +1,51 @@
 <template>
   <main>
     <button class="btn" disabled>Login into Warframe market</button>
-    <button class="btn btn-primary" @click="itemStore.getItems">Get list from Warframe market</button>
-    <p>There is total items in trade: {{ itemStore.items.length }}</p>
-    <p>Among them sets: {{ itemStore.sets.length }}</p>
-    <p>Warframes: {{ warframes.length }}</p>
-    <p>Weapons: </p>
-    <p>Others: </p>
-    <p>Companions: </p>
-    <p>Archwing: </p>
+
+    <button class="btn btn-primary" @click="getItems" :disabled="!isLoadComplete"> <span v-if="!isLoadComplete"
+        class="spinner-border spinner-border-sm me-2" aria-hidden="true"></span>Get list from Warframe market</button>
+
+
+    <table class="table table-striped-columns table-hover">
+      <tbody>
+
+        <tr>
+          <td>There is total items in trade</td>
+          <td> {{ itemStore.items.length }}</td>
+        </tr>
+
+        <tr>
+          <td>Among them sets</td>
+          <td> {{ itemStore.sets.length }}</td>
+        </tr>
+
+        <tr>
+          <td>Warframes</td>
+          <td> {{ warframes.length }}</td>
+        </tr>
+
+        <tr>
+          <td>Weapons</td>
+          <td> </td>
+        </tr>
+
+        <tr>
+          <td>Others</td>
+          <td> </td>
+        </tr>
+
+        <tr>
+          <td>Companions</td>
+          <td> </td>
+        </tr>
+
+        <tr>
+          <td>Archwing</td>
+          <td> </td>
+        </tr>
+      </tbody>
+
+    </table>
     <WarframeTable :warframes="warframes"></WarframeTable>
   </main>
 </template>
@@ -21,6 +58,12 @@ const itemStore = useTradeableItemStore()
 
 
 const warframes = ref(itemStore.warframes)
+const isLoadComplete = ref(true)
 
+const getItems = async () => {
+  isLoadComplete.value = false
+  await itemStore.getItems()
+  isLoadComplete.value = true
+}
 
 </script>
