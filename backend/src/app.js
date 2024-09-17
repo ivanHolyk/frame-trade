2ced2df48d082d13ec3bafc591cb3a08b6bd2098
ivan_holyk <ivan.Holyk369@gmail.com>
2024-09-17 14:25:52 +0300

    separated server startup from config

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import { config } from 'dotenv'
+config()
+
+import express, { json } from 'express'
+import morgan from 'morgan' // For logging HTTP requests
+import cors from 'cors' // For handling CORS requests
+import { router as apiRoutes } from './routes/v1/api.js'
+import { connect } from 'mongoose'
+import cookieParser from 'cookie-parser'
+
+export function createApp(db) {
+  const app = express()
+
+  app.use(json())
+  app.use(morgan('dev'))
+  app.use(cors())
+  app.use(cookieParser())
+
+  app.use('/api/v1', apiRoutes)
+
+  app.get('/', (req, res) => {
+    res.send('bruh, nice try')
+  })
+
+  connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log('Connected to MongoDB')
+    })
+    .catch((err) => {
+      console.error('Error connecting to MongoDB:', err.message)
+    })
+
+  return app
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,37 +1,11 @@
 import { config } from 'dotenv'
 config()
 
-import express, { json } from 'express'
-import morgan from 'morgan' // For logging HTTP requests
-import cors from 'cors' // For handling CORS requests
+import { createApp } from './app.js'
 
-const app = express()
 const port = process.env.PORT || 3000
-
-app.use(json())
-app.use(morgan('dev'))
-app.use(cors())
-
-import { router as apiRoutes } from './routes/v1/api.js'
-app.use('/api/v1', apiRoutes)
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
-
-import { connect } from 'mongoose'
-
-connect(
-  process.env.MONGODB_URI ||
-    'mongodb://localhost:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.0'
-)
-  .then(() => {
-    console.log('Connected to MongoDB')
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err.message)
-  })
-
-app.listen(port, () => {
+const app = createApp()
+const server = app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+export { server }
