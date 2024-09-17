d383332974432a9a40f5672084596a5b2328746e
ivan_holyk <ivan.Holyk369@gmail.com>
2024-09-17 14:29:13 +0300

    remove user and auth functionality

--- backend/src/routes/v1/api.js
+++ backend/src/routes/v1/api.js
@@ -1,36 +1,18 @@
 import { Router } from 'express'
 const router = Router()
 
-// Example route for fetching user inventory
-// router.get('/inventory', (req, res) => {
-//   res.json({ message: 'Fetching user inventory...' })
-// })
-
-// router.post('/inventory', (req, res) => {
-//   //check auth? skip for now
-
-//   //save item to db
-
-//   //return item
-
-//   res.json({ message: 'Fetching user inventory...' })
-// })
-
-//TODO finish later
-const userController = {}
-// router.get('/user', userController)
-
-import { auth } from '../../controllers/authController.js'
-router.post('/auth', auth)
-
 import { getInventory, addInventoryItem } from '../../controllers/inventoryController.js'
 import { authMiddleware } from '../../middleware/authMiddleware.js'
-router.get('/user/inventory', authMiddleware, getInventory)
-router.post('/user/inventory', authMiddleware, addInventoryItem)
+router
+  .route('/user/inventory')
+  .get(authMiddleware, getInventory)
+  .post(authMiddleware, addInventoryItem)
 
+//stub for now
 const marketController = { market: () => 'market' }
 router.get('/market', marketController.market)
 
+//stub for now
 const itemController = { getItems: () => ['power_drift', 'nyx_prime_set'] }
 router.get('/item', itemController.getItems)
 
@@ -40,8 +22,8 @@ export { router }
       /v1
         /user
           /inventory
-        /auth
-        /market 	basically proxy to warframe.market
-        /item		uses data from warframestats once in a month or so and caches it
-        
+          /wfmCredentials
+          /order  authorized warframe.market
+        /market 	basically proxy to warframe.market non authorized
+        /item		uses data from warframestats once in a month or so and caches it 
  */
