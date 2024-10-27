52a1b1097dff98c1ed181931b683afe755716a2f
ivan_holyk <ivan.Holyk369@gmail.com>
2024-10-27 22:39:37 +0200

    small changes to items vies

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,102 +1,127 @@
 <template>
-    <div>
-        <button @click="getRequestedItem()" class="btn btn-outline-secondary"><i
-                class="bi bi-arrow-clockwise"></i></button>
-        <!-- {{ $route.params.id }} -->
-        <!-- {{ item }} -->
-        <!-- {{ props }}
-        {{ $route.params }} -->
-        <div>
-            <div v-if="item" class="row">
-                <div class="col d-flex justify-content-center">
-                    <div>
+  <div>
+    <div v-if="item">
+      <v-row>
+        <!-- Item Image -->
+        <v-col class="d-flex justify-center">
+          <v-img :src="getIcon(item.icon)" alt="Item Icon" max-width="150"></v-img>
+        </v-col>
 
-                        <img :src="getIcon(item.icon)" alt="Item Icon" class="img-thumbnail">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <h3>{{ getItemName(item, currentLocale) }}</h3>
-                    <p><i class="bi bi-info-circle"></i> {{ getItemDescription(item, currentLocale) }}</p>
-                    <p><i class="bi bi-card-text"></i> Item id: {{ item.id }}</p>
-                    <p v-if="item.rarity"><i class="bi bi-star"></i> Rarity: {{ item.rarity }}</p>
-                    <p v-if="item.ducats">
-                        <DucatIcon /> {{ item.ducats }}
+        <!-- Item Details -->
+        <v-col>
+          <v-card elevation="1">
+            <v-card-title>
+              <h3>{{ getItemName(item, currentLocale) }}</h3>
+            </v-card-title>
 
-                    </p>
-                    <p><i class="bi bi-currency-exchange"></i> Trading Tax: {{ item.trading_tax }}</p>
-                    <p v-if="item.mod_max_rank >= 0"><i class="bi bi-arrow-bar-up"></i> Mod Max Rank: {{
-                        item.mod_max_rank
-                    }}
-                    </p>
-                    <p v-if="item.mastery_level"><i class="bi bi-award"></i> Mastery level: {{ item.mastery_level }}</p>
-                    <p v-if="item.set_root"><i class="bi bi-box"></i> Set root: {{ item.set_root }}</p>
-                    <p v-if="item.quantity_for_set"><i class="bi bi-123"></i> Quantity for set: {{ item.quantity_for_set
-                        }}</p>
-                    <p><i class="bi bi-image"></i> Icon format: {{ item.icon_format }}</p>
-                    <p><i class="bi bi-tags"></i> Tags:
-                        <span v-for="tag in item.tags" :key="tag" class="badge bg-secondary  me-1">
-                            {{ tag }}
-                        </span>
-                    </p>
-                    <p v-if="item.subtypes"><i class="bi bi-tag-fill"></i> Subtypes:
-                        <span v-for="tag in item.subtypes" :key="tag" class="badge bg-secondary me-1">
-                            {{ tag }}
-                        </span>
-                    </p>
-                    <p>
-                        <i class="bi bi-link-45deg"></i> Wiki Link:
-                        <a :href="getWikiLink(item, currentLocale)" target="_blank">
-                            {{ getWikiLink(item, currentLocale) }}
-                        </a>
-                    </p>
-                </div>
-            </div>
-            <div v-else>
-                <p>Loading item data...</p>
-            </div>
-        </div>
-        <RouterLink :to="{ name: 'itemOrders' }">Orders</RouterLink>
-        <RouterView></RouterView>
+            <v-card-subtitle>
+              <v-icon left>mdi-information</v-icon> {{ getItemDescription(item, currentLocale) }}
+            </v-card-subtitle>
+
+            <v-card-text>
+              <v-row>
+                <v-col>
+                  <p><v-icon left>mdi-card-text-outline</v-icon> Item ID: {{ item.id }}</p>
+                  <p v-if="item.rarity"><v-icon left>mdi-star</v-icon> Rarity: {{ item.rarity }}</p>
+                  <p v-if="item.ducats">
+                    <DucatIcon /> {{ item.ducats }}
+                  </p>
+                  <p><v-icon left>mdi-currency-usd</v-icon> Trading Tax: {{ item.trading_tax }}</p>
+                  <p v-if="item.mod_max_rank >= 0">
+                    <v-icon left>mdi-arrow-up-bold</v-icon> Mod Max Rank: {{ item.mod_max_rank }}
+                  </p>
+                  <p v-if="item.mastery_level">
+                    <v-icon left>mdi-award</v-icon> Mastery Level: {{ item.mastery_level }}
+                  </p>
+                  <p v-if="item.set_root">
+                    <v-icon left>mdi-box</v-icon> Set Root: {{ item.set_root }}
+                  </p>
+                  <p v-if="item.quantity_for_set">
+                    <v-icon left>mdi-numeric</v-icon> Quantity for Set: {{ item.quantity_for_set }}
+                  </p>
+                  <p><v-icon left>mdi-image-outline</v-icon> Icon Format: {{ item.icon_format }}</p>
+
+                  <!-- Tags -->
+                  <p><v-icon left>mdi-tag-outline</v-icon> Tags:</p>
+                  <v-chip-group>
+                    <v-chip v-for="tag in item.tags" :key="tag" color="secondary" outlined>{{
+                      tag
+                    }}</v-chip>
+                  </v-chip-group>
+
+                  <!-- Subtypes -->
+                  <p v-if="item.subtypes"><v-icon left>mdi-tag-multiple</v-icon> Subtypes:</p>
+                  <v-chip-group v-if="item.subtypes">
+                    <v-chip v-for="subtype in item.subtypes" :key="subtype" color="secondary" outlined>{{ subtype
+                      }}</v-chip>
+                  </v-chip-group>
+
+                  <!-- Wiki Link -->
+                  <p>
+                    <v-icon left>mdi-link</v-icon> Wiki Link:
+                    <a :href="getWikiLink(item, currentLocale)" target="_blank">
+                      {{ getWikiLink(item, currentLocale) }}
+                    </a>
+                  </p>
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card-actions>
+              <RouterLink :to="{ name: 'itemOrders' }">
+                <v-btn text>Orders</v-btn>
+              </RouterLink>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
     </div>
+
+    <!-- Loading State -->
+    <div v-else>
+      <v-progress-circular indeterminate></v-progress-circular>
+      <p>Loading item data...</p>
+    </div>
+
+    <RouterView></RouterView>
+  </div>
 </template>
 <script setup>
-import { useItemsStore } from '@/stores/item';
-import { ref, watch } from 'vue';
+import { useItemsStore } from '@/stores/item'
+import { ref, watch } from 'vue'
 
-import DucatIcon from '@/assets/DucatIcon.vue';
-import { RouterLink, RouterView } from 'vue-router';
+import DucatIcon from '@/assets/DucatIcon.vue'
+import { RouterLink, RouterView } from 'vue-router'
 const props = defineProps(['id', 'urlName'])
 
 const itemStore = useItemsStore()
 const item = ref()
-const getRequestedItem = (async () => {
-    let fetchedItem = await itemStore.getItem(props.urlName)
-    if (fetchedItem.items_in_set) {
-        item.value = fetchedItem.items_in_set.find(i => i.url_name === props.urlName)
-    } else {
-        item.value = fetchedItem
-    }
-})
+const getRequestedItem = async () => {
+  let fetchedItem = await itemStore.getItem(props.urlName)
+  if (fetchedItem.items_in_set) {
+    item.value = fetchedItem.items_in_set.find((i) => i.url_name === props.urlName)
+  } else {
+    item.value = fetchedItem
+  }
+}
 getRequestedItem()
 watch(props, (newUrl) => {
-
-    getRequestedItem()
+  getRequestedItem()
 })
 console.log(item.value)
 
 function getItemName(item, locale) {
-    return item[locale]?.item_name || item.en.item_name // Fallback to English if locale not found
+  return item[locale]?.item_name || item.en.item_name // Fallback to English if locale not found
 }
 
 function getItemDescription(item, locale) {
-    return item[locale]?.description || item.en.description // Fallback to English if locale not found
+  return item[locale]?.description || item.en.description // Fallback to English if locale not found
 }
 
 function getWikiLink(item, locale) {
-    return item[locale]?.wiki_link || item.en.wiki_link // Fallback to English if locale not found
+  return item[locale]?.wiki_link || item.en.wiki_link // Fallback to English if locale not found
 }
 function getIcon(url) {
-    //items/images/en/thumbs/limbo_prime_systems.7bfbc70bd8438594d45a47fba260b174.128x128.png
-    return `/static/assets/${url}`
+  //items/images/en/thumbs/limbo_prime_systems.7bfbc70bd8438594d45a47fba260b174.128x128.png
+  return `/static/assets/${url}`
 }
 </script>
--- src/views/ItemsView.vue
+++ src/views/ItemsView.vue
@@ -1,40 +1,36 @@
 <template>
-
-    <v-autocomplete v-model="selectedItemUrl" :label="'Items'" :placeholder="'Type to search...'" name="itemSearch"
-        append-icon="mdi-magnify" type="search" :items="itemOptions" :item-title="'text'" :item-value="'value'"
-        autocomplete></v-autocomplete>
-    <RouterView></RouterView>
-
-
+  <v-autocomplete elevation="1" v-model="selectedItemUrl" placeholder="Type to search..." label="Item"
+    prepend-inner-icon="mdi-magnify" :items="itemOptions" :item-title="'text'" :item-value="'value'"></v-autocomplete>
+  <RouterView></RouterView>
 </template>
 
 <script setup>
-import { useItemsStore } from '@/stores/item';
-import { ref, watch, computed } from 'vue';
-import { RouterView } from 'vue-router';
-import { watchDebounced } from '@vueuse/core';
-import router from '@/router';
+import { useItemsStore } from '@/stores/item'
+import { ref, watch, computed } from 'vue'
+import { RouterView } from 'vue-router'
+import { watchDebounced } from '@vueuse/core'
+import router from '@/router'
 
 const itemStore = useItemsStore()
-const selectedItemUrl = ref('')
+const selectedItemUrl = ref()
 const selectedItem = ref()
 
 // Prepare item options for the autocomplete
 const itemOptions = computed(() => {
-    return itemStore.items.map(item => ({
-        text: item.item_name,
-        value: item.url_name
-    }))
+  return itemStore.items.map((item) => ({
+    text: item.item_name,
+    value: item.url_name
+  }))
 })
 
 watchDebounced(
-    selectedItemUrl,
-    async (newUrl) => {
-        if (newUrl) {
-            selectedItem.value = await itemStore.getItem(newUrl, true)
-            router.push(`/item/${selectedItem.value.id}/${selectedItemUrl.value}`)
-        }
-    },
-    { debounce: 1000, maxWait: 2000 },
+  selectedItemUrl,
+  async (newUrl) => {
+    if (newUrl) {
+      selectedItem.value = await itemStore.getItem(newUrl, true)
+      router.push(`/item/${selectedItem.value.id}/${selectedItemUrl.value}`)
+    }
+  },
+  { debounce: 1000, maxWait: 2000 }
 )
 </script>
